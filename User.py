0a10c55cbac319b13ee444bbe65a88c505447e53
Christian Turner <keycoderk@gmail.com>
2023-04-04 17:28:52 -0400

    CRUD commit

--- User.py
+++ User.py
@@ -13,19 +13,44 @@ class User:
     @classmethod
     def get_all(cls):
         query = 'SELECT * FROM users'
-
         results = connectToMySQL(cls.db).query_db(query)
-
         users = []
-
         for user in results:
             users.append(cls(user))
         return users
 
     @classmethod
+    def get_one(cls,id):
+        query = 'SELECT * FROM users WHERE id = %(id)s'
+        result = connectToMySQL(cls.db).query_db(query,{'id' : id})
+        return cls(result[0])
+
+    @classmethod
     def create(cls,data):
         query = 'INSERT INTO users(first_name,last_name,email,created_at,updated_at) VALUES(%(first_name)s,%(last_name)s,%(email)s,NOW(),NOW())'
-
         results = connectToMySQL(cls.db).query_db(query,data)
-        print(results)
+        # print(results)
         return results
+
+    @classmethod
+    def update(cls,data):
+        query = """
+        UPDATE users 
+        SET
+        first_name=%(first_name)s,
+        last_name=%(last_name)s,
+        email=%(email)s,
+        updated_at=NOW()
+        WHERE id=%(id)s;
+        """
+        result = connectToMySQL(cls.db).query_db(query,data)
+        return result
+
+    @classmethod
+    def delete(cls,id):
+        query = """
+        DELETE FROM users
+        WHERE id=%(id)s;
+        """
+        result = connectToMySQL(cls.db).query_db(query,{'id' : id})
+        return result
--- server.py
+++ server.py
@@ -10,14 +10,45 @@ def index():
 
 @app.route('/create', methods=['POST'])
 def create():
-    User.create(request.form)
-    return redirect('/show')
+    user = User.create(request.form)
+    print(user)
+    return redirect('/show/' + str(user))
 
 @app.route('/show')
 def show():
     users = User.get_all()
-    print(users)
     return render_template('show.html', users=users)
+
+@app.route('/show/<int:id>')
+def show_one(id):
+    user = User.get_one(id)
+    return render_template('show_user.html', user=user)
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    user = User.get_one(id)
+    return render_template('edit.html', user=user)
+
+@app.route('/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    id = request.form['id']
+    return redirect('/show/' + str(id))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    user = User.get_one(id)
+    return render_template('delete.html', user=user)
+
+@app.route('/delete/confirmation', methods=['POST'])
+def confirmation():
+    if request.form['which_button'] == 'yes':
+        print('YES')
+        user = User.delete(request.form['id'])
+        return redirect('/show')
+    else:
+        print('NO')
+        return redirect('/show')
 
 if __name__ == '__main__':
     app.run(debug=True)
